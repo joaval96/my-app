9684e4cd7a02341999b3d306606f7b98a5a7bf27
joaquin valdivieso <joaval96@gmail.com>
2024-02-01 23:22:12 -0300

    Nombre del proyecto: my-app, Descripcion: version final

---
+++ src/main/java/com/joaval/myapp/entity/TestEntity.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+package com.joaval.myapp.entity;
+
+/**
+ * Author Joaquin Valdivieso
+ */
+public class TestEntity {
+	/*
+	 * Propiedad de prueba para la muestras de compilacion del archivo testentity.
+	 */
+	private String nombre;
+
+	/**
+	 * @return the nombre
+	 */
+	public String getNombre() {
+		return nombre;
+	}
+
+	/**
+	 * @param nombre the nombre to set
+	 */
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+
+}
